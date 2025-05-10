cf2fe5be5806ffde5c67fa8bc668c834bb4c5e24
malamaM <mchiluwe@gmail.com>
2025-05-10 12:25:58 +0200

    feat: implement dynamic fetching of houses for BestOffer and RecommendedHouse widgets

--- lib/widgets/best_offer.dart
+++ lib/widgets/best_offer.dart
@@ -1,42 +1,57 @@
 import 'package:flutter/material.dart';
-
 import 'package:house_rent/models/house.dart';
-import 'package:house_rent/widgets/circle_icon_button.dart';
+import 'package:house_rent/screens/details/details.dart';
 
-class BestOffer extends StatelessWidget {
-  final offerList = House.generateBestOffer();
+class BestOffer extends StatefulWidget {
+  const BestOffer({Key? key}) : super(key: key);
 
-  BestOffer({Key? key}) : super(key: key);
+  @override
+  _BestOfferState createState() => _BestOfferState();
+}
+
+class _BestOfferState extends State<BestOffer> {
+  late Future<List<House>> _offerList;
+
+  @override
+  void initState() {
+    super.initState();
+    _offerList = House.fetchHouses(); // Fetch houses dynamically
+  }
+
+  _handleNavigateToDetails(BuildContext context, House house) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Details(house: house),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Best Offer',
-                style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              Text(
-                'See All',
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      fontSize: 14,
-                    ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          ...offerList
-              .map(
-                (offer) => Container(
-                  margin: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.all(15),
+      child: SizedBox(
+        height: 340,
+        child: FutureBuilder<List<House>>(
+          future: _offerList,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No offers available'));
+            }
+
+            final offerList = snapshot.data!;
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: () =>
+                    _handleNavigateToDetails(context, offerList[index]),
+                child: Container(
+                  height: 300,
+                  width: 230,
                   padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -44,25 +59,28 @@ class BestOffer extends StatelessWidget {
                   ),
                   child: Stack(
                     children: [
-                      Row(
-                        children: [
-                          Container(
-                            width: 150,
-                            height: 80,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(offer.imageUrl),
-                                fit: BoxFit.cover,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              offerList[index].imageUrl,
                             ),
+                            fit: BoxFit.cover,
                           ),
-                          const SizedBox(width: 10),
-                          Column(
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          color: Colors.white54,
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                offer.name,
+                                offerList[index].name,
                                 style: Theme.of(context)
                                     .textTheme
                                     .displayLarge!
@@ -71,32 +89,29 @@ class BestOffer extends StatelessWidget {
                                       fontWeight: FontWeight.bold,
                                     ),
                               ),
-                              const SizedBox(height: 10),
+                              const SizedBox(height: 5),
                               Text(
-                                offer.address,
+                                offerList[index].address,
                                 style: Theme.of(context)
                                     .textTheme
                                     .bodyLarge!
                                     .copyWith(
-                                      fontSize: 14,
+                                      fontSize: 12,
                                     ),
                               ),
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                      const Positioned(
-                          right: 0,
-                          child: CircleIconButton(
-                            iconUrl: 'assets/icons/heart.svg',
-                            color: Colors.grey,
-                          ))
                     ],
                   ),
                 ),
-              )
-              .toList(),
-        ],
+              ),
+              separatorBuilder: (_, index) => const SizedBox(width: 20),
+              itemCount: offerList.length,
+            );
+          },
+        ),
       ),
     );
   }
--- lib/widgets/recommended_house.dart
+++ lib/widgets/recommended_house.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-
 import 'package:house_rent/models/house.dart';
 import 'package:house_rent/screens/details/details.dart';
 import 'package:house_rent/widgets/circle_icon_button.dart';
 
-class RecommendedHouse extends StatelessWidget {
-  final recommendedList = House.generateRecommended();
-
+class RecommendedHouse extends StatefulWidget {
   RecommendedHouse({Key? key}) : super(key: key);
+
+  @override
+  _RecommendedHouseState createState() => _RecommendedHouseState();
+}
+
+class _RecommendedHouseState extends State<RecommendedHouse> {
+  late Future<List<House>> _recommendedList;
+
+  @override
+  void initState() {
+    super.initState();
+    _recommendedList = House.fetchHouses(); // Fetch houses dynamically
+  }
 
   _handleNavigateToDetails(BuildContext context, House house) {
     Navigator.of(context).push(
@@ -23,88 +33,102 @@ class RecommendedHouse extends StatelessWidget {
       padding: const EdgeInsets.all(15),
       child: SizedBox(
         height: 340,
-        child: ListView.separated(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () =>
-                _handleNavigateToDetails(context, recommendedList[index]),
-            child: Container(
-              height: 300,
-              width: 230,
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          recommendedList[index].imageUrl,
+        child: FutureBuilder<List<House>>(
+          future: _recommendedList,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No houses available'));
+            }
+
+            final recommendedList = snapshot.data!;
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: () =>
+                    _handleNavigateToDetails(context, recommendedList[index]),
+                child: Container(
+                  height: 300,
+                  width: 230,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              recommendedList[index].imageUrl,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        fit: BoxFit.cover,
                       ),
-                    ),
-                  ),
-                  Positioned(
-                    right: 15,
-                    top: 15,
-                    child: CircleIconButton(
-                      iconUrl: 'assets/icons/mark.svg',
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      color: Colors.white54,
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      Positioned(
+                        right: 15,
+                        top: 15,
+                        child: CircleIconButton(
+                          iconUrl: 'assets/icons/mark.svg',
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          color: Colors.white54,
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                recommendedList[index].name,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .displayLarge!
-                                    .copyWith(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    recommendedList[index].name,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displayLarge!
+                                        .copyWith(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    recommendedList[index].address,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge!
+                                        .copyWith(
+                                          fontSize: 12,
+                                        ),
+                                  ),
+                                ],
                               ),
-                              const SizedBox(height: 5),
-                              Text(
-                                recommendedList[index].address,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge!
-                                    .copyWith(
-                                      fontSize: 12,
-                                    ),
+                              CircleIconButton(
+                                iconUrl: 'assets/icons/mark.svg',
+                                color: Theme.of(context).colorScheme.secondary,
                               ),
                             ],
                           ),
-                          CircleIconButton(
-                            iconUrl: 'assets/icons/mark.svg',
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
-          separatorBuilder: (_, index) => const SizedBox(width: 20),
-          itemCount: recommendedList.length,
+              separatorBuilder: (_, index) => const SizedBox(width: 20),
+              itemCount: recommendedList.length,
+            );
+          },
         ),
       ),
     );
